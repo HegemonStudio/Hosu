36567edd4b291ea722b3fe577370c621989b383d
Tymon Wozniak <tymon.student@gmail.com>
2025-11-01 09:24:45 +0000

    added colored logs using colorama lib

--- core/logger.py
+++ core/logger.py
@@ -1,16 +1,43 @@
 import logging
 import sys
+from colorama import init, Fore, Style
+
+# Initializes colorama (on Windows enables ANSI codes)
+init(autoreset=True)
+
+LEVEL_COLORS = {
+    logging.DEBUG: Fore.LIGHTBLACK_EX,
+    logging.INFO: Fore.WHITE,
+    logging.WARNING: Fore.YELLOW,
+    logging.ERROR: Fore.LIGHTRED_EX,
+    logging.CRITICAL: Fore.LIGHTMAGENTA_EX,
+}
+
+
+class ColoredFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        color = LEVEL_COLORS.get(record.levelno, "")
+        message = super().format(record)
+        return f"{color}{message}{Style.RESET_ALL}"
 
 
 def get_logger(name: str) -> logging.Logger:
+    """
+    Creates (or returns existing) logger with a standard stream handler.
+
+    :param name: Name of the logger
+
+    :return: Configured logger instance.
+    """
     logger = logging.getLogger(name)
 
+    # Prevent duplicate if get_logger is called multiple times
     if logger.handlers:
-        return logger  # logger has handler
+        return logger
 
     # Create handler for logger
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter(
+    formatter = ColoredFormatter(
         fmt="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
         datefmt="%H:%M:%S",
     )
